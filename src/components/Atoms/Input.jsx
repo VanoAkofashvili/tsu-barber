1274c0e5a7ec96ef9811903211ad44edcfe1cb12
Vaniko Akopashvili <v.akopashvili@sweeftdigital.com>
2022-05-28 22:46:30 +0400

    Fix input element error styles

--- src/components/Atoms/Input.jsx
+++ src/components/Atoms/Input.jsx
@@ -3,12 +3,18 @@ import { useField } from 'formik';
 export const Input = ({ label, ...props }) => {
   const [field, meta] = useField(props);
   return (
-    <div className="flex flex-col">
-      <label htmlFor={props.id || props.name}>{label}</label>
-      <input className="p-2 border border-gray-200" {...field} {...props} />
+    <div className="flex flex-col mb-6 relative">
+      <label htmlFor={props.id || props.name} className="text-base">
+        {label}
+      </label>
+      <input
+        className="p-2 border border-gray-200 placeholder:text-sm placeholder:italic"
+        {...field}
+        {...props}
+      />
       <div
-        className={`error ${
-          meta.touched && meta.error ? 'visible' : 'invisible'
+        className={`text-red-500 text-xs absolute top-full ${
+          meta.touched && meta.error ? 'opacity-100' : 'opacity-0'
         }`}
       >
         {meta.error}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,19 @@ module.exports = {
     fontFamily: {
       sans: ['IBM Plex Sans', 'sans-serif'],
     },
-    fill: {
-      current: 'currentColor',
-      black: '#121212',
-      primary: '#B7FF00',
-      mag: '#FF5C83',
-      transparent: 'rgba(0, 0, 0, 0)',
+
+    fontSize: {
+      xxs: '.7rem',
+      xs: '.75rem',
+      sm: '.875rem',
+      base: '1rem',
+      md: '1.125rem',
+      lg: '1.375rem',
+      xl: '1.75rem',
+      '2xl': '2rem',
+      '3xl': ['3rem', '3.250rem'],
+      '4xl': '4rem',
+      '5xl': '5rem',
     },
     extend: {
       backgroundImage: () => ({
